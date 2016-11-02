6e012805c0d7e9cc0d443ae796219f621e9a5e29
Johan Tez <jtesznar@gmail.com>
2016-11-02 19:43:15 +0000

    Updated brewu alias to remove the warning about the --all option

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -210,7 +210,7 @@ alias dbmd='spring rake db:migrate:down'
 alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
 
 # Postgres.app
 #alias psql='/Applications/Postgres.app/Contents/Versions/9.5/bin/psql -p5432'
